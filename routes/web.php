1889c101850b0bcaac7020964717870d8ec742c0
Aamir <amir.manzoor@immcorporate.com>
2024-07-30 17:29:13 +0500

    create laravel project

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('/amir', function () {
+//     session(['cart' => [
+//         [
+//             "item" => "RTX 10",
+//             "price" => 1000    
+//         ],
+//         [
+//             "item" => "RTX 11",
+//             "price" => 1000    
+//         ],
+//         [
+//             "item" => "RTX 12",
+//             "price" => 1000    
+//         ],
+//     ]]);
+//     $items = session()->get('cart');
+//     return $items   ;
+// });
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
